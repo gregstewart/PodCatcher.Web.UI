c20ce432cc8581e4813874fce3aa7194104f151d
Greg Stewart <gregs@tcias.co.uk>
2014-04-16 13:38:10 +0100

    Adding the main.js file to the repos here

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+    if (!options.crossDomain) {
+      options.crossDomain = true;
+    }
+
+    if (!options.xhrFields) {
+      options.xhrFields = {withCredentials:true};
+    }
+  });
+
+  var vent = _.extend({}, Backbone.Events);
+  new Podcatcher.ApplicationRouter({vent: vent});
+  Backbone.history.start();
+});
